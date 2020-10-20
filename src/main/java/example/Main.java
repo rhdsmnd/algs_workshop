e2f1770d194879abdbbde21d5fd1e812143f9f5e
Ron Desmond <rhdsmnd@gmail.com>
2020-10-20 06:17:05 -0400

    Updating default method run.

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -7,10 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        if (args.length > 0 && args[0].equals("LL")) {
-            testReverseLL();
-        } else {
+        if (args.length > 0 && args[0].equals("Filter")) {
             testFilterDups();
+        } else {
+            testReverseLL();
         }
     }
 
